1239fb65db103804d7d831e090b9bac2ebf1a6f8
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-15 13:08:35 -0500

    Added job edit functionality/form partial

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,6 +12,16 @@ class JobsController < ApplicationController
         redirect_to jobs_path
     end
 
+    def edit
+        @job = Job.find(params[:id])
+    end
+
+    def update
+        @job = Job.find(params[:id])
+        @job.update_attributes(job_params)
+        redirect_to jobs_path
+    end
+
     private
 
     def job_params
