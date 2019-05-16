5125e3bddf92bd3ce3061a8976a470d3d3b94440
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-16 18:21:02 -0500

    Added tags/tagging/migrated to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_210431) do
+ActiveRecord::Schema.define(version: 2019_05_16_231848) do
 
   create_table "jobs", force: :cascade do |t|
     t.text "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_taggings_on_job_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
