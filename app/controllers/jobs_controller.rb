9abfce144a580a92996628bd4a434ee038ca6d64
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-16 18:55:39 -0500

    Added tag_list to params/index & tags to form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -41,7 +41,7 @@ class JobsController < ApplicationController
     private
 
     def job_params
-        params.require(:job).permit(:title, :description)
+        params.require(:job).permit(:title, :description, :tag_list)
     end
 
     def sort_column
