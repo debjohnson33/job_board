dc7e7f42e416aaae2da064ffb820a4945e867538
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-16 19:42:29 -0500

    Added tag cloud to top of index & added css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
         direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
         link_to title, {:sort => column, :direction => direction}, {:class => css_class}
     end
+
+    def tag_cloud(tags, classes)
+        max = tags.sort_by(&:count).last
+        tags.each do |tag|
+           index = tag.count.to_f / max.count * (classes.size - 1)
+           yield(tag, classes[index.round]) 
+        end
+    end
 end
